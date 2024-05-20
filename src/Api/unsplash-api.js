52d1617c8ad9429ed53714d56b7025921cfc7d10
Hellwood1 <samburik123@gmail.com>
2024-05-20 18:59:15 +0200

    Upd . v.1.1
    - Інсталював axios/formik/yup.
    - Створив аккаунт на "unsplash"  та витягнув ключ доступк до API.

---
+++ src/Api/unsplash-api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.unsplash.com/";
+axios.defaults.headers = {
+    Authorization: "Client-ID jTHS3Vv6z-bXZfWLysjf1qTlFUOnpGH82vdOJRHowy0",
+};
+
+export default async function getPhotosByQuery (searchQuery, page) {
+    const response = await axios.get("search/photos", {
+        params: {
+            query: searchQuery,
+            per_page: 9,
+            page,
+        },
+    });
+
+    return response.data.results;
+}
